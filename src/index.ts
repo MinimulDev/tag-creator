9b1abce0b551ca38220327ca64bcf40e6c6665c5
Brendan Goldberg <goldberg.brendan@gmail.com>
2021-03-06 21:51:27 -0500

    chore: replace merge commit strategy

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,16 @@ new Promise(async () => {
     const skip_ci = core.getInput("token")
     const version_filename = core.getInput("version_file")
     const skip_ci_commit_string = core.getInput("skip_ci_commit_string")
+    const head_ref = core.getInput("ref")
     const owner = github.context.repo.owner
     const repo = github.context.repo.repo
+
+    if (!head_ref) {
+        core.info("attempted to run action not within context of a Pull Request")
+        return
+    } else {
+        core.info(`working with ${head_ref}`)
+    }
 
     if (token) {
         const input: Input = {
@@ -19,7 +27,8 @@ new Promise(async () => {
             repo: repo,
             skip_ci: skip_ci == "true",
             version_filename: version_filename,
-            skip_ci_commit_string: skip_ci_commit_string
+            skip_ci_commit_string: skip_ci_commit_string,
+            head_ref: head_ref
         }
 
         const manager = new ActionManager(input)
